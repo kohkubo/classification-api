116eac488a9e532c43b95d30526b4e3386f86448
kohkubo <kohkubo@student.42tokyo.jp>
2023-07-01 19:19:27 +0900

    pip化のため関数化

--- train/eval.py
+++ train/eval.py
@@ -9,17 +9,20 @@ from config import (
 )
 
 
-test_df = pd.read_csv(TEST_PATH, encoding="UTF-8")
-test_result_df = pd.read_csv(TEST_RESULT_PATH, encoding="UTF-8")
+def eval():
+    test_df = pd.read_csv(TEST_PATH, encoding="UTF-8")
+    test_result_df = pd.read_csv(TEST_RESULT_PATH, encoding="UTF-8")
 
-# テストデータの正解ラベルと予測ラベルを用いて混同行列をプロットする
-y_true = []
-y_pred = []
-for i in range(len(test_df)):
-    y_true.append(test_df["label"][i])
-    y_pred.append(test_result_df["label"][i])
+    # テストデータの正解ラベルと予測ラベルを用いて混同行列をプロットする
+    y_true = []
+    y_pred = []
+    for i in range(len(test_df)):
+        y_true.append(test_df["label"][i])
+        y_pred.append(test_result_df["label"][i])
 
-# クラス分類の評価指標を計算し、DataFrameに保存する
-report = classification_report(y_true, y_pred, target_names=LABELS, output_dict=True)
-report_df = pd.DataFrame(report).T
-print(report_df)
+    # クラス分類の評価指標を計算し、DataFrameに保存する
+    report = classification_report(
+        y_true, y_pred, target_names=LABELS, output_dict=True
+    )
+    report_df = pd.DataFrame(report).T
+    print(report_df)
--- train/train.py
+++ train/train.py
@@ -1,5 +1,3 @@
-# %%
-# 必要なライブラリをインポートする
 from transformers import TrainingArguments
 from transformers import Trainer
 from datasets import Dataset
@@ -15,99 +13,96 @@ from config import (
 )
 from model import get_device, get_model, get_tokenizer
 
-device = get_device()
-model = get_model(device)
-tokenizer = get_tokenizer()
 
-# %%
-# 訓練データと検証データを読み込む
-train_df = pd.read_csv("../text/news_train.csv", encoding="UTF-8")
-validation_df = pd.read_csv("../text/news_val.csv", encoding="UTF-8")
+def train():
+    device = get_device()
+    model = get_model(device)
+    tokenizer = get_tokenizer()
 
-# pandas DataFrameからDatasetオブジェクトを作成する
-train_dataset = Dataset.from_pandas(train_df)
-validation_dataset = Dataset.from_pandas(validation_df)
+    # %%
+    # 訓練データと検証データを読み込む
+    train_df = pd.read_csv("text/news_train.csv", encoding="UTF-8")
+    validation_df = pd.read_csv("text/news_val.csv", encoding="UTF-8")
 
-# DatasetDictオブジェクトを作成する
-dataset = DatasetDict(
-    {
-        "train": train_dataset,
-        "validation": validation_dataset,
-    }
-)
+    # pandas DataFrameからDatasetオブジェクトを作成する
+    train_dataset = Dataset.from_pandas(train_df)
+    validation_dataset = Dataset.from_pandas(validation_df)
 
+    # DatasetDictオブジェクトを作成する
+    dataset = DatasetDict(
+        {
+            "train": train_dataset,
+            "validation": validation_dataset,
+        }
+    )
 
-# %%
-# トークナイザ処理
-def tokenize(batch):
-    return tokenizer(batch["sentence"], padding=True, truncation=True)
+    # %%
+    # トークナイザ処理
+    def tokenize(batch):
+        return tokenizer(batch["sentence"], padding=True, truncation=True)
 
+    # Datasetオブジェクトをトークナイズする
+    dataset_encoded = dataset.map(tokenize, batched=True, batch_size=None)
 
-# Datasetオブジェクトをトークナイズする
-dataset_encoded = dataset.map(tokenize, batched=True, batch_size=None)
+    # %%
+    # 評価指標の定義
+    def compute_metrics(pred):
+        labels = pred.label_ids
+        preds = pred.predictions.argmax(-1)
+        f1 = f1_score(labels, preds, average="weighted")
+        acc = accuracy_score(labels, preds)
+        return {"accuracy": acc, "f1": f1}
 
+    # %%
+    # 学習の準備
+    batch_size = 16
+    logging_steps = len(dataset_encoded["train"]) // batch_size
+    model_name = "classification"
+    training_args = TrainingArguments(
+        output_dir=model_name,
+        num_train_epochs=2,
+        learning_rate=2e-5,
+        per_device_train_batch_size=batch_size,
+        per_device_eval_batch_size=batch_size,
+        weight_decay=0.01,
+        evaluation_strategy="epoch",
+        disable_tqdm=False,
+        logging_steps=logging_steps,
+        push_to_hub=False,
+        log_level="error",
+    )
 
-# %%
-# 評価指標の定義
-def compute_metrics(pred):
-    labels = pred.label_ids
-    preds = pred.predictions.argmax(-1)
-    f1 = f1_score(labels, preds, average="weighted")
-    acc = accuracy_score(labels, preds)
-    return {"accuracy": acc, "f1": f1}
+    # %%
+    # 学習
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        compute_metrics=compute_metrics,
+        train_dataset=dataset_encoded["train"],
+        eval_dataset=dataset_encoded["validation"],
+        tokenizer=tokenizer,
+    )
 
+    trainer.train()
 
-# %%
-# 学習の準備
-batch_size = 16
-logging_steps = len(dataset_encoded["train"]) // batch_size
-model_name = "classification"
-training_args = TrainingArguments(
-    output_dir=model_name,
-    num_train_epochs=2,
-    learning_rate=2e-5,
-    per_device_train_batch_size=batch_size,
-    per_device_eval_batch_size=batch_size,
-    weight_decay=0.01,
-    evaluation_strategy="epoch",
-    disable_tqdm=False,
-    logging_steps=logging_steps,
-    push_to_hub=False,
-    log_level="error",
-)
+    # %%
+    # 検証データで予測を行う
+    preds_output = trainer.predict(dataset_encoded["validation"])
 
-# %%
-# 学習
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    compute_metrics=compute_metrics,
-    train_dataset=dataset_encoded["train"],
-    eval_dataset=dataset_encoded["validation"],
-    tokenizer=tokenizer,
-)
+    # 予測結果を取得する
+    y_preds = np.argmax(preds_output.predictions, axis=1)
+    y_valid = np.array(dataset_encoded["validation"]["label"])
 
-trainer.train()
+    # %%
+    # 評価指標を表示する
+    print(f"Accuracy: {accuracy_score(y_valid, y_preds)}")
+    print(f"F1: {f1_score(y_valid, y_preds, average='weighted')}")
+    print(f"Confusion Matrix: \n{confusion_matrix(y_valid, y_preds)}")
+    print(f"Classification Report: \n{classification_report(y_valid, y_preds)}")
+    print(f"Labels: {LABELS}")
+    print(f"Predictions: {y_preds}")
+    print(f"True Values: {y_valid}")
 
-# %%
-# 検証データで予測を行う
-preds_output = trainer.predict(dataset_encoded["validation"])
-
-# 予測結果を取得する
-y_preds = np.argmax(preds_output.predictions, axis=1)
-y_valid = np.array(dataset_encoded["validation"]["label"])
-
-
-# %%
-# 評価指標を表示する
-print(f"Accuracy: {accuracy_score(y_valid, y_preds)}")
-print(f"F1: {f1_score(y_valid, y_preds, average='weighted')}")
-print(f"Confusion Matrix: \n{confusion_matrix(y_valid, y_preds)}")
-print(f"Classification Report: \n{classification_report(y_valid, y_preds)}")
-print(f"Labels: {LABELS}")
-print(f"Predictions: {y_preds}")
-print(f"True Values: {y_valid}")
-
-# %%
-# モデルを保存する
-trainer.save_model(MODEL_PATH)
+    # %%
+    # モデルを保存する
+    trainer.save_model(MODEL_PATH)
